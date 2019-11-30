846a9f2e0ef829d2f12da61d908465124f67c4c9
Salamander333 <y.glushkov@abv.bg>
2019-11-30 14:45:10 +0200

    Added basic database fillers

---
+++ WorksShopInitial/TemporaryStartUp/DatabaseFillers/AddArmors.cs
@@ -0,0 +1,38 @@
+﻿namespace TemporaryStartUp.DatabaseFillers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using WorkShop.Data;
+    using WorkShop.Models;
+
+    public class AddArmors
+    {
+        private Dictionary<string, int> Armors = new Dictionary<string, int>()
+        { 
+            { "Bronze Plate", 100},
+            { "Gold Plate", 150},
+            { "Iron Plate", 200},
+            { "Chainmail Plate", 250},
+            { "Titanium Plate", 300}
+        };
+
+        public void CreateArmors(WorkShopContext context)
+        {
+            foreach (var Armor in Armors)
+            {
+                var armor = new Armor()
+                {
+                    Name = Armor.Key,
+                    Defence = Armor.Value,
+                    Durability = 3
+                };
+
+                context.Armors.Add(armor);
+                context.SaveChanges();
+
+                Console.WriteLine($"Succesfully added armor {armor.Name}");
+            }
+        }
+    }
+}
---
+++ WorksShopInitial/TemporaryStartUp/DatabaseFillers/AddWeapons.cs
@@ -0,0 +1,37 @@
+﻿namespace TemporaryStartUp.DatabaseFillers
+{
+    using System;
+    using System.Collections.Generic;
+    using WorkShop.Data;
+    using WorkShop.Models;
+
+    public class AddWeapons
+    {
+        private Dictionary<string, int> Weapons = new Dictionary<string, int>()
+        {
+            { "Dagger", 100},
+            { "Iron Sword", 150},
+            { "Flail", 200},
+            { "Spear", 250},
+            { "Poleaxe", 300}
+        };
+
+        public void CreateWeapons(WorkShopContext context)
+        {
+            foreach (var Weapon in Weapons)
+            {
+                var weapon = new Weapon()
+                {
+                    Name = Weapon.Key,
+                    Damage = Weapon.Value,
+                    Durability = 3,
+                };
+
+                context.Weapons.Add(weapon);
+                context.SaveChanges();
+
+                Console.WriteLine($"Succesfully added weapon {weapon.Name}");
+            }
+        }
+    }
+}
